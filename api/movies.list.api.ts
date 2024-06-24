fb76795e900447d5f72bd81d5921d7d6db188ed2
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 00:22:30 +0700

    initial fetchMoviesByTopRated API

---
+++ api/movies.list.api.ts
@@ -0,0 +1,13 @@
+import { API_KEY, BASE_URL } from "@/constants/env";
+
+export const fetchMoviesByTopRated = async () => {
+    const apiUrl = BASE_URL + '/movie/top_rated?api_key=' + API_KEY;
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    const responseBody = res.json();
+    return responseBody;
+
+}
