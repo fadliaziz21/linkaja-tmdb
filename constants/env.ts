3d426f82c9675c6dba5962420d54504f16195cba
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 00:19:08 +0700

    initial env const

---
+++ constants/env.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://api.themoviedb.org/3'
+export const API_KEY = (process.env.API_KEY as string);
+export const IMG_URL = 'https://media.themoviedb.org/t/p/w220_and_h330_face';
