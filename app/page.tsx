32e5ca63092dd2999244552f21a42e564cca0c2b
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 10:25:59 +0700

    update html styling and indentation

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import Card from "@/components/card/Card";
 export default async function Home() {
   const data = await fetchMoviesByTopRated();
 
-  return (
-    <>
-      <div className="relative p-8">
-        <div className="grid grid-cols-8 gap-5">
-          {
-            data.results.map( (item: any, index: number) => (
-              <Card key={index} data={item} />
-            ))
-          }
-        </div>
-      </div>
-    </>
+    return (
+        <>
+            <div className="relative p-8">
+                <div className="grid grid-cols-8 gap-5">
+                    {
+                        data.results.map( (item: any, index: number) => (
+                            <Card key={index} data={item} />
+                        ))
+                    }
+                </div>
+            </div>
+        </>
   );
 }
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div className='flex justify-between items-center p-3 max-w-6xls mx-auto bg-[#032541]'>
-      <div className='flex items-center gap-1'>
-        <Link href={'/'} className='flex gap-1 items-center'>
-          <span className='text-2xl text-gray-100 font-bold bg-[#01b4e4] py-1 px-2 rounded-lg'>
-            The Movie Database
-          </span>
-        </Link>
-      </div>
-    </div>
-  )
+    return (
+        <div className="flex justify-between items-center p-3 max-w-6xls mx-auto bg-[#032541]">
+            <div className="flex items-center gap-1">
+                <Link href={`/`} className="flex gap-1 items-center">
+                    <span className="text-2xl text-gray-100 font-bold bg-[#01b4e4] py-1 px-2 rounded-lg">
+                        The Movie Database
+                    </span>
+                </Link>
+            </div>
+        </div>
+    )
 }
