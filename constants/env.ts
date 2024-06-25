3910bb0ffd59dd0a937802287c264cf950024bdb
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 14:19:34 +0700

    update BASE_URL constant

--- constants/env.ts
+++ constants/env.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.themoviedb.org/3'
+export const BASE_URL = 'https://api.themoviedb.org/3';
 export const API_KEY = (process.env.API_KEY as string);
 export const IMG_ORI_URL = 'https://image.tmdb.org/t/p/original/';
 export const IMG_PREV_URL = 'https://media.themoviedb.org/t/p/w220_and_h330_face';
