80bba65020fdebc45c4e8817c1c81825da09740e
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 10:22:08 +0700

    update Card components layout

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,23 +1,22 @@
-import { IMG_URL } from '@/constants/env'
+import { IMG_PREV_URL } from '@/constants/env'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Card(props: any) {
     return (
         <>
-            <div key={props.data.id} className="relatives rounded-xl bg-gray-100 bg-clip-border text-gray-700 shadow-md py-3">
-                <div className="relative mx-2 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
-                    <img
-                        src={IMG_URL + props.data.poster_path}
-                    />
-                </div>
-                <div className="p-2">
-                    <div className="mb-2 flex items-center justify-between">
-                        <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+            <Link href={`/movie/${props.data.id}`}>
+                <div key={props.data.id} className="relatives rounded-xl bg-white bg-clip-border">
+                    <div className="relative overflow-hidden rounded-xl bg-white bg-clip-border shadow-md">
+                        <img src={IMG_PREV_URL + props.data.poster_path} />
+                    </div>
+                    <div className="my-2 flex items-center justify-between">
+                        <b className="block font-sans text-sm font-medium leading-relaxed">
                             {props.data.title}
-                        </p>
+                        </b>
                     </div>
                 </div>
-            </div>
+            </Link> 
         </>
     )
 }
