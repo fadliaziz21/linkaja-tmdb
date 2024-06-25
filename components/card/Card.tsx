6c4baf60feac68df73bdce0a36ed26feab9002b6
LAPTOP-VAVPR0TB\fadli <fadli.aziz@anabatic.com>
2024-06-25 14:20:12 +0700

    update Card component layout

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -6,12 +6,12 @@ export default function Card(props: any) {
     return (
         <>
             <Link href={`/movie/${props.data.id}`}>
-                <div key={props.data.id} className="relatives rounded-xl bg-white bg-clip-border">
-                    <div className="relative overflow-hidden rounded-xl bg-white bg-clip-border shadow-md">
+                <div key={props.data.id}>
+                    <div className="relative overflow-hidden rounded-xl bg-clip-border shadow-md">
                         <img src={IMG_PREV_URL + props.data.poster_path} />
                     </div>
                     <div className="my-2 flex items-center justify-between">
-                        <b className="block font-sans text-sm font-medium leading-relaxed">
+                        <b className="block font-sans text-sm font-medium text-white leading-relaxed">
                             {props.data.title}
                         </b>
                     </div>
